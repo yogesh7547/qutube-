8e99642d2c09fdd735c94982fb5ab3a2151acaf1
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-09-09 16:38:57 +0530

    removed the mongodb connection notes for cleanliness of code

--- src/index.js
+++ src/index.js
@@ -14,11 +14,7 @@ connectDB();
 
 
 
-//dotenv is a package that loads environment variables from a .env file into your Node.js app
 
-
-//dotenv.config() reads your .env file and loads those variables into memory so they're available as process.env.WHATEVER.
-//you need to call it before any code (like connectDB function ) that tries to use those environment variables.
 
 
 
