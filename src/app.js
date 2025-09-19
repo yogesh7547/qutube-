7b220c5508519b9f1fc0ce9bb5d98079f348cf56
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-09-19 14:37:32 +0530

    removed utils notes for cleanliness

--- src/app.js
+++ src/app.js
@@ -31,26 +31,3 @@ export { app };
 
 
 
-// Express framework - think of it as importing a toolkit that makes it easy to create web servers.
-// CORS (Cross-Origin Resource Sharing) - a security feature that controls which websites can talk to your server.
-//cookie parser- imports a tool that helps your server understand and work with cookies (small pieces of data stored in users' browsers).
-//Raw cookies come to the server as one long string like: "sessionId=abc123; theme=dark; lang=en"
-//Cookie-parser converts this into an easy-to-use format:
-/* {
-  sessionId: "abc123",
-  theme: "dark", 
-  lang: "en"
-} */
-
-
-  // CORS_ORIGIN is an environment variable (a setting stored outside your code) that contains the web address of the ONLY website you want to allow access to your server.
-
-//app.use(express.json({ limit: "16kb" }));
-//This lets your server understand JSON data (a common data format) sent by clients, but limits it to 16 kilobytes to prevent huge uploads that could slow down your server.
-
-//app.use(express.static("public"));
-//Static files are files that don't change or process - they're served exactly as they are stored on your server:
-//Without static serving: Every time someone wants an image, they have to ask the cashier (server) to go to the back room, find it, and bring it to them.
-//With static serving: You put commonly needed items in a display case(public folder) where customers can directly pick them up without bothering staff.
-//These URLs become automatically available:
-//http://yoursite.com/images/logo.png → serves public/images/logo.png
--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -20,30 +20,6 @@ export {asyncHandler}
 
 
 
-// higher-order function (a function that takes another function as input and returns a new function) designed to handle async/await errors in Express.js routes automatically.
-
-
-
-//FLOW
-// 1. Route called: app.get('/users/:id', asyncHandler(yourAsyncFunction))
-
-// 2. Express calls: wrappedFunction(req, res, next)
-
-// 3. wrappedFunction calls: Promise.resolve(yourAsyncFunction(req, res, next))
-
-// 4a. If success: Response sent normally
-// 4b. If error: .catch() triggers → next(error) → Error middleware handles it
-
-
-
- //.catch((err) => next(err))
-        // ^^^       ^^^^^^^^
-        //  |         |
-        //  |         └─ Pass error to Express error handler
-        //  └─ The error that was thrown
-
-
-
 
 
 
