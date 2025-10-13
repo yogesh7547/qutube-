d7d311fd9f7a005f010470ad0ce357f4b64fd5a4
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-10-13 17:25:45 +0530

    add updateAvatar route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from '../controllers/user.controller.js';
+import { loginUser, logoutUser, registerUser,refreshAccessToken, updateUserAvatar } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,6 +24,10 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/updateAvatar").post(
+    upload.single("avatar"),
+    updateUserAvatar
+)
 
 export default router
 
