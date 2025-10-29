2629d95251c09983b8c60c065ec2b05ff8d4f87d
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-10-29 09:10:12 +0530

    fix tweet model

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -4,7 +4,11 @@ const tweetSchema= new Schema({
    content:{
     type:String,
     required:true
-   } 
+   },
+   owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 },{timestamps:true})
 
 export const Tweet= mongoose.model("Tweet",tweetSchema)
