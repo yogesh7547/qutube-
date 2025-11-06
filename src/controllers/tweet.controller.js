f07e2d7f60f966c327aaff96faa84e427f693758
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-11-07 00:46:50 +0530

    add tweet controller

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,127 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createTweet = asyncHandler(async (req, res) => {
+  //TODO: create tweet
+
+  const { content } = req.body;
+
+  if (!content.trim()) {
+    throw new ApiError(400, "tweet content is required");
+  }
+
+  const tweet = await Tweet.create({
+    content: content,
+    owner: req.user._id,
+  })
+
+  if (!tweet) {
+    throw new ApiError(500, "Failed to create the tweet");
+  }
+
+  const populatedTweet= await tweet.populate("owner", "username avatar")
+
+  
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, populatedTweet, "tweet created successfully"));
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  // TODO: get user tweets
+  
+  const {userId}= req.params;
+  const user= await User.findById(userId)
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }     
+
+  const tweets= await Tweet.aggregate([
+    {
+        $match:{
+            owner: new mongoose.Types.ObjectId(userId)
+        },
+    },
+    {
+        $sort:{
+            createdAt:-1
+        }
+    },
+    {
+        $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField:"_id",
+            as:"owner",
+            pipeline:[
+                {
+                    $project:{
+                        username:1,
+                        avatar:1
+                    }
+                }
+            ]
+        }
+    },
+    {
+        $addFields:{
+            owner:{
+                $first:"$owner"
+            }
+        }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, tweets, "user's tweets fetched successfully"))
+
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  //TODO: update tweet
+  const {newContent}= req.body
+  const {tweetId}= req.params
+  
+  if(!newContent||!newContent.trim()){
+    throw new ApiError(400, "Content is required")
+  }
+
+  const tweet= await Tweet.findById(tweetId)
+
+  if(!tweet){
+    throw new ApiError(404, "tweet not found")
+  }
+
+  tweet.content=newContent;
+
+  const updatedTweet= await tweet.save();
+   
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,updatedTweet,"Successfully updated the tweet"))
+
+  
+
+  
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  //TODO: delete tweet
+  const {tweetId}=req.params;
+  const tweet= await Tweet.findById(tweetId)
+  await tweet.deleteOne()
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,{},"successfully deleted the Tweet"))
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,18 +1,21 @@
-import mongoose,{mongo, Schema} from "mongoose";
+import mongoose, { mongo, Schema } from "mongoose";
 
-const tweetSchema= new Schema({
-   content:{
-    type:String,
-    required:true
-   },
-   likes:{
-    type:Number,
-    default:0
-   },
-   owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-},{timestamps:true})
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Tweet= mongoose.model("Tweet",tweetSchema)
+export const Tweet = mongoose.model("Tweet", tweetSchema);
