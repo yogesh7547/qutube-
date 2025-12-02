731b083dcd82822920f7f058a6a69f1b5f45b8fb
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-12-02 16:00:29 +0530

    add new route declarations

--- src/app.js
+++ src/app.js
@@ -18,13 +18,25 @@ app.use(cookieParser());
 
 //routes import 
 import userRouter from './routes/user.routes.js'
+import likeRouter from './routes/like.routes.js'
+import tweetRouter from './routes/tweet.routes.js'
+import commentRouter from './routes/comment.routes.js'
+import videoRouter from './routes/video.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
 
 
 //routes declaration
 app.use("/api/v1/users",userRouter)// base path
+app.use("/api/v1/like",likeRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/playlists",playlistRouter)
 
 
 export { app };
+
+
 
 
 
