9ce377e284f654f375b82ee14a019798ec55a663
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-12-02 18:27:59 +0530

    add subscription route declaration

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import tweetRouter from './routes/tweet.routes.js'
 import commentRouter from './routes/comment.routes.js'
 import videoRouter from './routes/video.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
 
 
 //routes declaration
@@ -32,6 +33,7 @@ app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/subscriptions",subscriptionRouter)
 
 
 export { app };
